a1eae4133dc0eabada436e1301368b0a0616b2dc
alejandro <alejandro@alejandro-pc>
2013-12-12 21:03:00 +0000

    Refactored GUI methods

--- src/minesweeper/view/GameFrame.java
+++ src/minesweeper/view/GameFrame.java
@@ -2,12 +2,7 @@ package minesweeper.view;
 
 import java.awt.GridLayout;
 import java.awt.HeadlessException;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
 
 public final class GameFrame extends JFrame {
 
@@ -25,50 +20,7 @@ public final class GameFrame extends JFrame {
 
     public void createComponents() {
         GameFrameMenuBar menuBar = new GameFrameMenuBar();
-        JMenu gameMenu = menuBar.createMenu("Game");
-        JMenu difficultySubmenu = menuBar.createMenu("Difficulty");
-        JMenu helpMenu = menuBar.createMenu("Help");
-
-        JMenuItem newGameItem = menuBar.createMenuItem("New game...");
-        JMenuItem easyDifficultyItem = menuBar.createMenuItem("Easy");
-        JMenuItem mediumDifficultyItem = menuBar.createMenuItem("Medium");
-        JMenuItem hardDifficultyItem = menuBar.createMenuItem("Hard");
-        JMenuItem exitItem = menuBar.createMenuItem("Exit");
-        JMenuItem howToPlayItem = menuBar.createMenuItem("How to play?");
-        JMenuItem aboutItem = createAboutButton();
-
-        // Game menu
-        gameMenu.add(newGameItem);
-        difficultySubmenu.add(easyDifficultyItem);
-        difficultySubmenu.add(mediumDifficultyItem);
-        difficultySubmenu.add(hardDifficultyItem);
-        gameMenu.add(difficultySubmenu);
-        gameMenu.add(exitItem);
-
-        // Help menu
-        helpMenu.add(howToPlayItem);
-        helpMenu.add(aboutItem);
-
-        menuBar.add(gameMenu);
-        menuBar.add(helpMenu);
-
-        frame.add(menuBar);
-
-    }
-
-    public JMenuItem createAboutButton() {
-        JMenuItem button = new JMenuItem("About...");
-        button.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(frame,
-                        "This game was created by Alejandro Perez Martin.\n" + "(GitHub @alejandroperezmartin)\n" + "\nVersion 1.0.1",
-                        "About Minesweeper...",
-                        JOptionPane.PLAIN_MESSAGE);
-            }
-        });
-        return button;
+        frame.add(menuBar.getMenuBar());
     }
 
     public void createMenuBar() {
--- src/minesweeper/view/GameFrameMenuBar.java
+++ src/minesweeper/view/GameFrameMenuBar.java
@@ -5,12 +5,59 @@ import java.awt.event.ActionListener;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
-public class GameFrameMenuBar extends JMenuBar {
+public final class GameFrameMenuBar extends JMenuBar {
 
-    public JMenuBar GameFrameMenuBar() {
-        JMenuBar menuBar = new JMenuBar();
-        return menuBar;
+    private final JMenuBar menuBar;
+
+    public GameFrameMenuBar() {
+        this.menuBar = new JMenuBar();
+        createComponens();
+    }
+
+    public void createComponens() {
+        JMenu gameMenu = createMenu("Game");
+        JMenu difficultySubmenu = createMenu("Difficulty");
+        JMenu helpMenu = createMenu("Help");
+
+        JMenuItem newGameItem = createMenuItem("New game...");
+        JMenuItem easyDifficultyItem = createMenuItem("Easy");
+        JMenuItem mediumDifficultyItem = createMenuItem("Medium");
+        JMenuItem hardDifficultyItem = createMenuItem("Hard");
+        JMenuItem exitItem = createMenuItem("Exit");
+        JMenuItem howToPlayItem = createMenuItem("How to play?");
+        JMenuItem aboutItem = createAboutItem();
+
+        // Game menu
+        gameMenu.add(newGameItem);
+        difficultySubmenu.add(easyDifficultyItem);
+        difficultySubmenu.add(mediumDifficultyItem);
+        difficultySubmenu.add(hardDifficultyItem);
+        gameMenu.add(difficultySubmenu);
+        gameMenu.add(exitItem);
+
+        // Help menu
+        helpMenu.add(howToPlayItem);
+        helpMenu.add(aboutItem);
+
+        menuBar.add(gameMenu);
+        menuBar.add(helpMenu);
+    }
+
+    public JMenuItem createAboutItem() {
+        final JMenuItem aboutItem = new JMenuItem("About...");
+        aboutItem.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(aboutItem,
+                        "This game was created by Alejandro Perez Martin.\n" + "(GitHub @alejandroperezmartin)\n" + "\nVersion 1.0.1",
+                        "About Minesweeper...",
+                        JOptionPane.PLAIN_MESSAGE);
+            }
+        });
+        return aboutItem;
     }
 
     public JMenuItem createMenuItem(String title) {
@@ -27,6 +74,10 @@ public class GameFrameMenuBar extends JMenuBar {
 
     public JMenu createMenu(String title) {
         return new JMenu(title);
+    }
+
+    public JMenuBar getMenuBar() {
+        return menuBar;
     }
 
 }
